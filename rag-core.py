4422e3ae09dde6860f1828193a354cde6be4068d
invst-git <invst.zero@gmail.com>
2026-05-02 00:42:55 +0530

    added core modules for the RAG pipeline

--- rag-core.py
+++ rag-core.py
@@ -6,3 +6,15 @@ from langchain_anthropic import ChatAnthropic
 from langchain.prompts import PromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_document_chain
+# loading
+loader = PyPDFLoader("Documents\Analysis1.pdf")
+documents = loader.load()
+# chunking
+splitter = RecursiveCharacterTextSplitter(chunk_size=1024,chunk_overlap=256)
+chunks = splitter.split_documents(documents)
+# embeddings
+embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+# using FAISS to keep the vectorDB simple for this implementation
+vectordb = FAISS.from_documents(documents=chunks,embeddings=embeddings)
+#retriever
+retriever = vectordb.as_retriever()
